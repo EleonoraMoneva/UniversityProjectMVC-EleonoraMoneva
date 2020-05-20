acb9d3f2a9d94965d3a0480c83c0a2616f16f265
Eleonora Moneva <58509263+EleonoraMoneva@users.noreply.github.com>
2020-05-20 22:11:08 +0200

    Add files via upload

---
+++ UniversityProject/Controllers/CourseApiController.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityProject.Data;
+using UniversityProject.Models;
+
+namespace UniversityProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseApiController : ControllerBase
+    {
+        private readonly datacontext _context;
+        public CourseApiController(datacontext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/CourseApi
+        [HttpGet]
+        public IEnumerable<Course> GetCourse()
+        {
+            return _context.Course;
+        }
+
+        //GET: api/CourseApi/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCourse([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var course = await _context.Course.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return Ok(course);
+        }
+        // PUT: api/CourseApi/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse([FromRoute] int id, [FromBody] Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != course.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        // POST: api/CourseApi
+        [HttpPost]
+        public async Task<IActionResult> PostCourse([FromBody] Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Course.Add(course);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCourse), new { id = course.Id }, course);
+        }
+
+        // DELETE: api/CourseApi/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var course = await _context.Course.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Course.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return Ok(course);
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Course.Any(e => e.Id == id);
+        }
+    }
+}
